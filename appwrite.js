e8ab34e5ff9cbb31f9a98705462a4cf27e57162d
boopathiraja-06 <boopathiraja060700@gmail.com>
2025-06-17 13:00:43 +0530

    almost done

---
+++ appwrite.js
@@ -0,0 +1,37 @@
+import { Client,Databases,ID,Query } from "appwrite";
+const DATABASE_ID=import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID=import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+
+const PROJECT_ID =import.meta.env.VITE_APPWRITE_PROJECT_ID
+
+const client=new Client()
+.setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setProject(PROJECT_ID)
+  
+  const database=new Databases(client);
+
+export const updateSearchCount=async (searchTerm,movie)=>{
+    //use appwrite  to check if term searchterm exist in database
+    try{
+        const result=await database.listDocuments(DATABASE_ID,COLLECTION_ID,[Query.equal('searchTerm',searchTerm),])
+        //if it does exist,update count
+        if(result.documents.length>0){
+            const doc=result.documents[0];
+
+            await database.updateDocument(DATABASE_ID,COLLECTION_ID,doc.$id,{count:doc.count+1,})
+        } 
+        // if it doesn't create  new and count 1
+        else{
+            await database.createDocument(DATABASE_ID,COLLECTION_ID,ID.unique(),{
+                searchTerm,
+                count:1,
+                movieid:movie.id,
+                 poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+
+            })
+        }
+    }catch (error) {
+  console.error(error);
+ }
+    
+}
