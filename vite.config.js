1f247e19d2b6ce5113b43d0b228b6a0041aef9e6
boopathiraja-06 <boopathiraja060700@gmail.com>
2025-06-17 14:31:15 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './', // <- this makes asset paths relative (important for Netlify)
+  base: '/movielisting/', // <- this makes asset paths relative (important for Netlify)
 })
